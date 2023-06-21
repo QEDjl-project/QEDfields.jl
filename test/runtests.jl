e86a8824d2d482ef2a32fe3b5139b7d6ec87ecfb
Uwe Hernandez Acosta <xkj@gmx.de>
2023-06-21 10:56:29 +0200

    added QEDbase 0.1.4 as a dependency and dummy tests to check if QEDbase is installed; this status is reflected in the README

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
 using QEDfields
 using Test
+import Pkg
+
+
+function isinstalled(pk::AbstractString)
+  return pk in [v.name for v in values(Pkg.dependencies())]
+end
+
 
 @testset "QEDfields.jl" begin
     # Write your tests here.
+    
+    @test isinstalled("QEDbase")
+
 end
