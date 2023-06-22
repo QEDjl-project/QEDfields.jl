60324ad5c8101130417a8109a9f0984869ce45a6
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2023-06-22 13:28:36 +0200

    dummy tests  (#2)
    In this PR adds `QEDbase=0.1.4` to the dependencies and a dummy
    function, which checks if `QEDbase` is installed. This status is also
    reflected in the README.
    
    The commit resulting from merging this PR is supposed to be tagged as
    `v0.1.0-dev` and can be registered.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # QEDfields
 
 [![Code Style: Blue](https://img.shields.io/badge/code%20style-blue-4495d1.svg)](https://github.com/invenia/BlueStyle)
+
+
+:warning: This package is under construction and only contains dummy functionality for testing.
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using QEDfields
 using Test
+import Pkg
+
+
+function isinstalled(pk::AbstractString)
+  return pk in [v.name for v in values(Pkg.dependencies())]
+end
+
 
 @testset "QEDfields.jl" begin
     # Write your tests here.
+    
+    @testset "Integration: QEDbase" begin
+      
+      @test isinstalled("QEDbase")
+      @test length(dummy_QEDbase(rand(4)))==4
+    end
+
+
 end
