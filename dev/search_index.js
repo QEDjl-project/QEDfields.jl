b5afa42571684e1b8f50625216e4e17b4195ccfd
Documenter.jl <documenter@juliadocs.github.io>
2023-10-10 12:53:59 +0000

    build based on 747163b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QEDfields","category":"page"},{"location":"#QEDfields","page":"Home","title":"QEDfields","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QEDfields.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QEDfields]","category":"page"}]
+}
