b62b8b9b773dc2d9dffd0f8efdc4330ef0482c8a
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-11-01 15:04:13 +0100

    adjusted polarisations.jl

--- src/polarisation.jl
+++ src/polarisation.jl
@@ -1,9 +1,21 @@
+#################
+# polarisation vectors
+# 
+# In this file, we provide an extension of the Polarisations of Photons from
+# QEDbase to define polarisation states of background fields
+################
+#
+# TODO:
+#   * write doc strings
+#   * implement elliptical/circular polarisation
 
-
-@inline polarisation_vector(pol::AbstractDefinitePolarization, mom) = _photon_state(pol, mom)
-@inline polarisation_vector(pol::AbstractIndefinitePolarization, mom) = _photon_state(mom)
+@inline polarisation_vector(pol::AbstractDefinitePolarization, mom) = base_state(Photon(),Incoming(), mom, pol)
 
 @inline _oscillator(::PolX, x) = cos(x)
 @inline _oscillator(::PolY, x) = sin(x)  
-@inline _oscillator(x) = (cos(x),sin(x))
-@inline _oscillator(::AbstractIndefinitePolarization,x) = _oscillator(x)
+@inline function _oscillator(::AbstractIndefinitePolarization,x) 
+    sincos_res = sincos(x)
+    @inbounds cossin_res = (sincos_res[2], sincos_res[1])
+    return cossin_res
+end
+
