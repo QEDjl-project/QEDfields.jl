2c8d099686822f1d06464c11d584666841799dd8
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-11-13 11:27:10 +0100

    fixed typo in docstring

--- src/interfaces/background_field_interface.jl
+++ src/interfaces/background_field_interface.jl
@@ -42,6 +42,7 @@ Interface function for [`AbstractPulsedPlaneWaveField`](@ref), which returns the
 function reference_momentum end
 
 """
+
     domain(::AbstractPulsedPlaneWaveField)
 
 Interface function for [`AbstractPulsedPlaneWaveField`](@ref), which returns interval (as a `IntervalSets.Interval`) for the given background field. 
