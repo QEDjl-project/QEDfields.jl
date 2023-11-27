224e6d8fe2ec7e5781de7e8d2cf672cbd4ebbafa
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-11-27 23:06:07 +0100

    added comment for polarization vectors

--- src/polarization.jl
+++ src/polarization.jl
@@ -15,6 +15,11 @@
 Return the polarization vector for a given polarization and four-momentum `mom`.
 For a definite polarization, the respective `LorentzVector` is returned, 
 where as for an indefinite polarization, a tuple of polarization vectors is returned.
+
+!!! note "Convention"
+
+    In the current implementation, we use the `base_state` function for `Photon` provided by `QEDbase`.
+
 """
 @inline function polarization_vector(pol::AbstractPolarization, mom)
     return base_state(Photon(), Incoming(), mom, pol)
