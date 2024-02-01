19385dd7aa5c672b690ec5a99aa6cf8a7dccdc76
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-02-01 20:23:20 +0100

    formatting

--- src/pulses/cos_square_pulse.jl
+++ src/pulses/cos_square_pulse.jl
@@ -18,17 +18,18 @@ Concrete implementation of an `AbstractPulsedPlaneWaveField` for cos-square puls
     for \$\\phi\\in (-\\Delta\\phi,\\Delta\\phi),\$where \$\\Delta\\phi`\$ denotes the `pulse_width`, and zero otherwise.
 
 """
-struct CosSquarePulse{M<:QEDbase.AbstractFourMomentum,T<:Real} <: AbstractPulsedPlaneWaveField
+struct CosSquarePulse{M<:QEDbase.AbstractFourMomentum,T<:Real} <:
+       AbstractPulsedPlaneWaveField
     mom::M
     pulse_width::T
 end
 
-@inline function _unsafe_cos_square_envelope(phi,dphi) 
-    return cos(pi*phi/(2*dphi))^2
+@inline function _unsafe_cos_square_envelope(phi, dphi)
+    return cos(pi * phi / (2 * dphi))^2
 end
 
-function _cos_square_envelope(x,dphi)
-    -dphi<= x <= dphi ? unsafe_envelope(x,dphi) : zero(x)
+function _cos_square_envelope(x, dphi)
+    return -dphi <= x <= dphi ? unsafe_envelope(x, dphi) : zero(x)
 end
 
 ####
@@ -38,31 +39,32 @@ end
 reference_momentum(pulse::CosSquarePulse) = pulse.mom
 function domain(pulse::CosSquarePulse)
     delta_phi = pulse.pulse_width
-    return Interval(-delta_phi,delta_phi) 
+    return Interval(-delta_phi, delta_phi)
 end
 
 phase_duration(pulse::CosSquarePulse) = pulse.pulse_width
-_envelope(pulse::CosSquarePulse,phi::Real) = _unsafe_cos_square_envelope(phi,pulse.pulse_width)
+function _envelope(pulse::CosSquarePulse, phi::Real)
+    return _unsafe_cos_square_envelope(phi, pulse.pulse_width)
+end
 
 #######
 # Special implementation for the generic spectrum
 #######
 
 @inline function _gsinc(x::T) where {T<:Real}
-    abs(x) == 1 ? one(x) / 2 : sinc(x) / (1 - x^2)
+    return abs(x) == 1 ? one(x) / 2 : sinc(x) / (1 - x^2)
 end
 
 @inline function _generic_FT(l::Real, sig::Real)
-    sig * _gsinc(sig * l / pi)
+    return sig * _gsinc(sig * l / pi)
 end
-
 
 function generic_spectrum(field::CosSquarePulse, pol::PolX, pnum::Real)
     dphi = field.pulse_width
-    0.5 * (_generic_FT(pnum + 1, dphi) + _generic_FT(pnum - 1, dphi))
+    return 0.5 * (_generic_FT(pnum + 1, dphi) + _generic_FT(pnum - 1, dphi))
 end
 
 function generic_spectrum(field::CosSquarePulse, pol::PolY, pnum::Real)
     dphi = field.pulse_width
-    -0.5im * (_generic_FT(pnum + 1, dphi) - _generic_FT(pnum - 1, dphi))
+    return -0.5im * (_generic_FT(pnum + 1, dphi) - _generic_FT(pnum - 1, dphi))
 end
