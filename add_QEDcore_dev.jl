ad8f6b9a5a5615d13e8048b5f6f18078a2b39fc3
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2024-06-20 11:41:30 +0200

    Use base_state from core (#30)
    With this PR, QEDfields should use `base_state` from QEDcore, not from
    QEDbase.
    
    This is part of the general restructuring of `QED.jl`, see
    https://github.com/QEDjl-project/QED.jl/issues/35 for details.
    
    ---------
    
    Co-authored-by: Uwe Hernandez Acosta <u.hernandez@hzdr.de>

--- add_QEDcore_dev.jl
+++ add_QEDcore_dev.jl
@@ -3,3 +3,4 @@
 
 using Pkg: Pkg
 Pkg.add(; url="https://github.com/QEDjl-project/QEDcore.jl", rev="dev")
+#Pkg.add(; url="https://github.com/QEDjl-project/QEDbase.jl.git", rev="process_interfaces")
--- test/downstream.jl
+++ test/downstream.jl
@@ -2,6 +2,8 @@
 # check if upstream packages provide the assumed functionality
 ###################
 
+# TODO: remove this because it is covered by the integration tests
+
 using Pkg: Pkg
 
 # QEDbase.jl
