dddd5aa2dde23d0cf8de0d970705567c16569754
Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
2024-06-28 22:12:27 +0200

    Refac cleanup (#34)
    The namespace shadowing of QEDbase is revoked. This is part of the
    restructuring of `QED.jl`. See
    https://github.com/QEDjl-project/QED.jl/issues/35 for details.
    
    ---------
    
    Co-authored-by: Uwe Hernandez Acosta <u.hernandez@hzdr.de>

--- src/interfaces/background_field_interface.jl
+++ src/interfaces/background_field_interface.jl
@@ -113,16 +113,14 @@ end
 # amplitude functions
 
 function _amplitude(
-    field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
-    phi::Real,
+    field::AbstractPulsedPlaneWaveField, pol::AbstractDefinitePolarization, phi::Real
 )
     return oscillator(pol, phi) * _envelope(field, phi)
 end
 
 function _amplitude(
     field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
+    pol::AbstractDefinitePolarization,
     phi::AbstractVector{T},
 ) where {T<:Real}
     # TODO: maybe use broadcasting here 
@@ -150,16 +148,14 @@ Returns the value of the amplitude for a given polarization direction and phase 
     the value of the amplitude is returned, and zero otherwise.
 """
 function amplitude(
-    field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
-    phi::Real,
+    field::AbstractPulsedPlaneWaveField, pol::AbstractDefinitePolarization, phi::Real
 )
     return phi in domain(field) ? _amplitude(field, pol, phi) : zero(phi)
 end
 
 function amplitude(
     field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
+    pol::AbstractDefinitePolarization,
     phi::AbstractVector{T},
 ) where {T<:Real}
     # TODO: maybe use broadcasting here 
@@ -191,16 +187,14 @@ Return the generic spectrum of the given field, for the given polarization direc
     where ``g(\\phi)`` is the [`envelope`](@ref) and ``l`` the photon number parameter.
 """
 function generic_spectrum(
-    field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
-    pnum::Real,
+    field::AbstractPulsedPlaneWaveField, pol::AbstractDefinitePolarization, pnum::Real
 )
     return _fourier_transform(t -> _amplitude(field, pol, t), domain(field), pnum)
 end
 
 function generic_spectrum(
     field::AbstractPulsedPlaneWaveField,
-    pol::QEDbase.AbstractDefinitePolarization,
+    pol::AbstractDefinitePolarization,
     photon_number_parameter::AbstractVector{T},
 ) where {T<:Real}
     # TODO: maybe use broadcasting here 
